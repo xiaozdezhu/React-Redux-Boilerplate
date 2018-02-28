4ab6b5188b0f26608ef069f638d5987acc844a58
Angelina <06angelina19@gmail.com>
2018-02-28 00:43:14 -0500

    commit

--- dev/js/reducers/reducer-active-user.js
+++ dev/js/reducers/reducer-active-user.js
@@ -6,7 +6,7 @@
 // "state = null" is set so that we don't throw an error when app first boots up
 export default function (state = null, action) {
     switch (action.type) {
-        case 'USER_SELECTED_test':
+        case 'USER_SELECTED':
             return action.payload;
             break;
     }
